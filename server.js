2cbd567b08bbdf112ae8aafc9aae31f0fdea45f4
yanhackcode15 <iamyanhu@gmail.com>
2018-03-02 13:28:38 -0800

    ihhjlj

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
+app.get('*', (req, res)=>{ res.sendfile('index.html', {root: __dirname + '/dist'})});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,7 +37,11 @@ const routes: Routes = [
 	{
 		path: 'photo-gallery',
 		component: PhotoGalleryComponent,
-	}
+	},
+	{
+		path: '**', 
+		component: LandingPageComponent,
+	},
 ];
 
 @NgModule({
